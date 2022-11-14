bf3e87955c301e36938e34e3e8ff29f74c83b177
Samuel Ife <samuelife@hotmail.co.uk>
2022-11-14 14:51:11 +0000

    implemented 2d arraylist

--- src/main/java/oop/blueprints/Grid.java
+++ src/main/java/oop/blueprints/Grid.java
@@ -1,39 +1,52 @@
 package oop.blueprints;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Grid {
     int width;
-    int length;
-    ArrayList<Cells> cells = new ArrayList<>();
+    int height;
+    int totalCells;
+    ArrayList<Cells>[][] cells;
 
     // creates initial grid, all cells set to 'unopened'
-    public Grid(int width, int length){
+    public Grid(int width, int height){
         this.width = width;
-        this.length = length;
+        this.height = height;
+        this.cells = new ArrayList[width][height];
         for (int row = 1; row <= this.width; row++){
-            for (int column = 1; column <= this.length; column++){
+            for (int column = 1; column <= this.height; column++){
                 // for each coordinate (row, column), creates new cell object, adds to arraylist
                 Cells cell = new Cells(row, column);
+
+                // "row/column - 1" so that user input gets converted to index
+                this.cells[row-1][column-1] = new ArrayList<>();
+                this.cells[row-1][column-1].add(cell);            // adds cell to 2d arraylist
+                cell = this.cells[row-1][column-1].get(0);        // retrieves cell from arraylist
                 System.out.print(cell.appearance);
-                this.cells.add(cell);
             }
             System.out.print("\n");
         }
-
+        this.totalCells = this.width * this.height;
     }
 
     public void gridBuilder(){
-        int index = 0;
         for (int row = 1; row <= this.width; row++){
-            for (int column = 1; column <= this.length; column++){
-                // for each coordinate (row, column), creates new cell object, adds to arraylist
-                Cells cell = this.cells.get(index);
+            for (int column = 1; column <= this.height; column++){
+                // for each coordinate (row, column), gets cell from 2d arraylist
+                Cells cell = this.cells[row-1][column-1].get(0);
                 System.out.print(cell.appearance);
-                index++;
             }
             System.out.print("\n");
         }
+    }
+
+    public void setMines(){
+        Random rand = new Random();
+        //for ()
+        rand.nextInt(this.totalCells);
+
 
     }
+
 }
